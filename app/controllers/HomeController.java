e3a43e3ed57ba604a7173f218416a0fe92d0c6b7
iTAP <iTAP@iTAP-PC13>
2016-05-26 12:18:31 +0900

    Javaスクリプトによる絞り込みの成功！

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -30,4 +30,29 @@ public class HomeController extends Controller {
     	return ok(test.render(gc,cs));
     }
 
-}
+    public Result creation() {
+    	///DEPARTMENT DT = DEPARTMENT.find.byId(1);
+    	//CATEGORY CG = CATEGORY.find.byId(1);
+    	//return ok(test.render(gc,cs));
+    	return ok(creation.render());
+    }
+
+    public Result kanri() {
+        return ok(kanri.render());
+
+    }
+    public Result valuation_detailed() {
+        return ok(valuation_detailed.render());
+    }
+    public Result sanp() {
+        return ok(sanp.render());
+    }
+
+    public Result select_js(){
+    	return ok(views.js.selectEmp.select.render());
+    }
+    public Result sanpl2() {
+        return ok(sanpl2	.render());
+    }
+    }
+
