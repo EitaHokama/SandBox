c5d88e15b8d2d358aac5b0e61e94d412577b928a
iTAP <iTAP@iTAP-PC12>
2016-05-31 09:45:21 +0900

    transとreceiveのコントローラーとファイル位置の変更

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -81,41 +81,6 @@ public class HomeController extends Controller {
     	newCard.find.all();
     	return redirect(routes.HomeController.test());
     }
-    public Result receive(){
-    	List<Gratitude_Card> gc ;
 
-    	Map<String, String[]> params =request().body().asFormUrlEncoded();
-    	SelectGC sel = new SelectGC(params);
-    	gc = sel.findRec(Employees.find.byId(1));//Employeesクラスを代入・現在は社員1でログインしていると仮定
-
-    	return ok(receive.render(gc, "",params));
-    }
-    public Result receiveGet(){
-    	List<Gratitude_Card> gc ;
-    	Map<String, String[]> params =new HashMap<String, String[]>();
-
-    	SelectGC sel = new SelectGC(params);
-    	gc = sel.findRec(Employees.find.byId(1));//Employeesクラスを代入・現在は社員1でログインしていると仮定
-
-    	return ok(receive.render(gc, "",params));
-    }
-    public Result trans(){
-    	List<Gratitude_Card> gc ;
-
-    	Map<String, String[]> params =request().body().asFormUrlEncoded();
-    	SelectGC sel = new SelectGC(params);
-    	gc = sel.findSend(Employees.find.byId(1));//Employeesクラスを代入・現在は社員1でログインしていると仮定
-
-    	return ok(trans.render(gc, "",params));
-    }
-    public Result transGet(){
-    	List<Gratitude_Card> gc ;
-    	Map<String, String[]> params =new HashMap<String, String[]>();
-
-    	SelectGC sel = new SelectGC(params);
-    	gc = sel.findSend(Employees.find.byId(1));//Employeesクラスを代入・現在は社員1でログインしていると仮定
-
-    	return ok(trans.render(gc, "",params));
-    }
 }
 
